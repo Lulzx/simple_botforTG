d2c3219fe2b8ea0d558571ecc544038c5a3bcbd5
Lulzx <wiserishabh@gmail.com>
2017-09-07 14:22:10 +0530

    Update config.php

--- config.php
+++ config.php
@@ -9,12 +9,12 @@
 
     // TELEGRAM API    
     define("TELEGRAM_API_URL", "https://api.telegram.org/bot");
-    define("TELEGRAM_API", TELEGRAM_API_URL . TELEGRAM_API_TOKEN . "/");
+    define("TELEGRAM_API", https://lulzx.000webhostapp.com/simplerobot.php . 399618513:AAHDms9llRVhcxvc0LFhq0x-PBGw4p2QcZg . "/");
 
     // PERSONALITY
-    define("BOT_NAME", "Bob");
+    define("BOT_NAME", "Lulzx");
     define("BOT_GENDER", "m");
-    define("BOT_BIRTH", "04.09.2015");  // dd.mm.YYYY
+    define("BOT_BIRTH", "04.09.2017");  // dd.mm.YYYY
 
     // LOCALIZATION
     define("TIMEZONE", "+1");
